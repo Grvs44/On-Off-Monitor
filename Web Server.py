8bef8424f85337bbb20e6e40cc53124ec981ada6
Joe Greaves <74414406+JoeGreaves17@users.noreply.github.com>
2021-03-16 14:43:32 +0000

    Day 2
    Changed some status codes
    Sorted out shut down
    Changed the project name

--- Web Server.py
+++ Web Server.py
@@ -1,57 +1,62 @@
-from socket import *
-mainpath = ""
+from socket import socket,AF_INET,SOCK_STREAM,SHUT_WR
+mainpath = "grvs.co.uk"
 def createServer():
     serversocket = socket(AF_INET, SOCK_STREAM)
-    try :
-        serversocket.bind(('localhost',9000))
-        serversocket.listen(5)
-        shutdown = True
-        while(1):
-                (clientsocket, address) = serversocket.accept()
-                rd = clientsocket.recv(5000).decode()
-                pieces = rd.split("\n")
-                path = ""
-                if ( len(pieces) > 0 ) :
-                    print(pieces[0])
-                    path = pieces[0].split(" ")[1].lower()
-                print(path)
-                #data = "HTTP/1.1 200 OK\r\n"
-                #data += "Content-Type: text/html; charset=utf-8\r\n"
-                #data += "\r\n"
-                data = "" # Stores location for 302s
-                contenttype = "text/html"
-                httpcode = 200
-                if(path == "/shutdown" and shutdown == False):
-                    data = "<title>Shut down</title><p>The device has shut down</p>"
-                    shutdown = True
-                elif path == "/localdata":
-                    f=open("locallog.dgl",mode="r")
-                    data = f.read()
-                    f.close()
-                elif path == "/log.csv":
-                    if mainpath == "" : # This device is the main device
-                        f = open("log.csv",mode="r")
-                        data=f.read()
-                        f.close()
-                        contenttype="text/csv"
-                    else : # This device is not the main device
-                        data="http://"+mainpath+"/log.csv"
-                        httpcode=302 # https://en.wikipedia.org/wiki/HTTP_302
-                else:
-                    f = open("HomePage.html",mode='r')
+    serversocket.bind(('localhost',9000))
+    serversocket.listen(5)
+    shutdown = False
+    firstrun = True
+    while(1):
+            (clientsocket, address) = serversocket.accept()
+            rd = clientsocket.recv(5000).decode()
+            pieces = rd.split("\n")
+            path = ""
+            if ( len(pieces) > 0 ) :
+                print(pieces[0])
+                try: path = pieces[0].split(" ")[1].lower()
+                except IndexError : pass
+            print(path)
+            #data = "HTTP/1.1 200 OK\r\n"
+            #data += "Content-Type: text/html; charset=utf-8\r\n"
+            #data += "\r\n"
+            data = "" # Stores location for 302s
+            contenttype = "text/html"
+            httpcode = 200
+            if path == "/":
+                f = open("HomePage.html",mode='r')
+                data=f.read()
+                f.close()
+            elif path == "/shutdown" and not firstrun:
+                data = "<title>Shut down - Monitor</title><h1 style='font-family:\"Segoe UI\";text-align:center'>The web service has shut down</h1>"
+                shutdown = True
+            elif path == "/localdata":
+                f=open("locallog.dgl",mode="r")
+                data = f.read()
+                f.close()
+            elif path == "/log.csv":
+                if mainpath == "" : # This device is the main device
+                    f = open("log.csv",mode="r")
                     data=f.read()
                     f.close()
-                    shutdown = False
-                #data += "\r\n\r\n"
-                if httpcode == 200:clientsocket.sendall(("HTTP/1.1 200 OK\r\nContent-Type: "+contenttype+"; charset=utf-8\r\n\r\n"+data+"\r\n\r\n").encode())
-                elif httpcode == 302: clientsocket.sendall(("HTTP/1.1 302 FOUND\r\nLocation: "+data+"\r\n\r\n").encode())
-                clientsocket.shutdown(SHUT_WR)
-                if(shutdown):break
-    except KeyboardInterrupt :
-        print("\nShutting down...\n")
+                    contenttype="text/csv"
+                else : # This device is not the main device
+                    data="http://"+mainpath+"/log.csv"
+                    httpcode=302 # https://en.wikipedia.org/wiki/HTTP_302
+            else:
+                data = "/"
+                if path == "/shutdown" : httpcode = 302
+                else : httpcode = 301
+                #shutdown = False
+            #data += "\r\n\r\n"
+            if httpcode == 200:clientsocket.sendall(("HTTP/1.1 200 OK\r\nContent-Type: "+contenttype+"; charset=utf-8\r\n\r\n"+data+"\r\n\r\n").encode())
+            elif httpcode == 301: clientsocket.sendall(("HTTP/1.1 301 MOVED\r\nLocation: "+data+"\r\n\r\n").encode())
+            elif httpcode == 302: clientsocket.sendall(("HTTP/1.1 302 FOUND\r\nLocation: "+data+"\r\n\r\n").encode())
+            clientsocket.shutdown(SHUT_WR)
+            if(shutdown):break
+            firstrun = False
     serversocket.close()
 def GetData():
-    mysock = socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock = socket(AF_INET,SOCK_STREAM)
     mysock.connect(('data.pr4e.org', 80))
     cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
     mysock.send(cmd)
