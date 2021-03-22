082573036f888b3c8a07ff678ade031b4c4dccc2
JoeGreaves17 <74414406+JoeGreaves17@users.noreply.github.com>
2021-03-22 15:14:52 +0000

    Day 6
    Started custom shutdown and log file download
    Web server settings are not used from the file

--- Web Server.py
+++ Web Server.py
@@ -4,16 +4,18 @@ class Settings():
     mainpath = ""
     devices = []
     port = 80
-    def __init__(self):#https://www.w3schools.com/python/python_classes.asp
+    def __init__(self):
+        self.devices = []
         f = open("ServerSettings.dat","rb")
         self = pickle.load(f)
         f.close()
-def CreateServer():
+def Server():
     serversocket = socket(AF_INET, SOCK_STREAM)
     serversocket.bind(('localhost',settings.port))
     serversocket.listen(5)
     shutdown = False
     firstrun = True
+    turnoff = False
     while(1):
             (clientsocket, address) = serversocket.accept()
             rd = clientsocket.recv(5000).decode()
@@ -24,7 +26,7 @@ def CreateServer():
                 try: path = pieces[0].split(" ")[1].lower()
                 except IndexError : pass
             print(path)
-            data = "" # Stores location for 302s
+            data = ""
             contenttype = "text/html"
             httpcode = 200
             if path == "/":
@@ -32,7 +34,28 @@ def CreateServer():
                 data=f.read()
                 f.close()
             elif path == "/shutdown" and not firstrun:
-                data = "<title>Shut down - On/Off Monitor</title><h1 style='font-family:\"Segoe UI\";text-align:center'>The web service has shut down</h1>"
+                pathdata = pieces[len(pieces)-1].split("&")
+                devices = "this"
+                web = "web"
+                data = "<title>Shut down - On/Off Monitor</title><h1 style='font-family:\"Segoe UI\";text-align:center'>"
+                for i in range(len(pathdata)):
+                    pathdata[i] = pathdata[i].split("=")
+                    if pathdata[i][0] == "devices":
+                        devices = pathdata[i][1]
+                    elif pathdata[i][0] == "web":
+                        web = pathdata[i][1]
+                if devices == "all":
+                    if settings.mainpath == "":
+                        print(settings.devices)
+                        for device in settings.devices:
+                            GetData(device,"/shutdown",postlist=["web",web])
+                            print("a")
+                    else: GetData(settings.mainpath,"/shutdown",postlist=[["devices",devices],["web",web]])
+                    data+="Shut down requested for all devices</h1>"
+                else:
+                    data+="Shut down</h1>"
+                if web == "all":
+                    turnoff = True
                 shutdown = True
             elif "/localdata" in path:
                 try:
@@ -45,13 +68,15 @@ def CreateServer():
                 except IndexError: pass
             elif path == "/log.csv":
                 if settings.mainpath == "" : # This device is the main device
-                    f = open("log.csv",mode="r")
-                    data=f.read()
-                    f.close()
+                    filedata = []
+                    for i in range(lognum):
+                        for device in devices:
+                            data.append(pickle.load(GetData(device,"/localdata?"+str(lognum)))) ##
+                    
                     contenttype="text/csv"
                 else : # This device is not the main device
                     data="http://"+settings.mainpath+"/log.csv"
-                    httpcode=302 # https://en.wikipedia.org/wiki/HTTP_302
+                    httpcode=302
             else:
                 data = "/"
                 if path == "/shutdown" : httpcode = 302
@@ -63,10 +88,15 @@ def CreateServer():
             if(shutdown):break
             firstrun = False
     serversocket.close()
-def GetData():
+    if turnoff: print("Turned off")
+def GetData(address,path,postlist=[]):
+    post = ""
+    for i in range(len(postlist)):
+        post+=postlist[i][0]+"="+postlist[i][1]
+        if i+1 != len(postlist): post+="&"
     mysock = socket(AF_INET,SOCK_STREAM)
-    mysock.connect(('data.pr4e.org', 80))
-    cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+    mysock.connect((address, 80))
+    cmd = ('GET http://'+address+path+' HTTP/1.0\r\n\r\n'+post).encode()
     mysock.send(cmd)
     iterations = 0
     while True:
@@ -84,8 +114,7 @@ def GetLogFileList():
         logfiles = pickle.load(g)
         g.close()
     except FileNotFoundError: print("FNF")
-#print('Access http://localhost:9000')
 logfiles = []
 settings = Settings()
 GetLogFileList()
-CreateServer()
+Server()
