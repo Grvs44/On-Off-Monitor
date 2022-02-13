e546072300ec7a243441682b9d7b9de4548b3fcb
JoeGreaves17 <74414406+JoeGreaves17@users.noreply.github.com>
2022-02-13 11:32:40 +0000

    Added update extra log conditions repository. The repository path to be pulled is stored in ExtraLogFolder.txt

--- countdown.sh
+++ countdown.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo "On/Off Monitor: Checking for updates..."
 git pull
+if test -f "ExtraLogFolder.txt"; then
+    folder=$PWD
+    extralogfolder=$(<ExtraLogFolder.txt)
+    cd "$extralogfolder"
+    echo "On/Off Monitor: Checking for extra log conditions updates..."
+    git pull
+    cd "$folder"
+fi
 echo "On/Off Monitor will start in 5 seconds. Press Ctrl-C to exit"
 sleep 5
 sudo python3 .
